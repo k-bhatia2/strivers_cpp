cf68934b67741acb22b4962d811955737dffc4fe
k-bhatia2 <kshitijbhatia02@gmail.com>
2024-12-12 13:20:45 +0530

    Patterns

--- pattern2.cpp
+++ pattern2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 void print1(int n){
      for(int i=0; i<n; i++){
-        for(int j=0; j<i; j++){
+        for(int j=0; j<=i; j++){
             cout<<"*";
         }
         cout<<endl;
