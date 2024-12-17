18a782e06eebf47dc8ee84173b74b6c906882195
k-bhatia2 <kshitijbhatia02@gmail.com>
2024-12-17 20:45:18 +0530

    iterators iteratitons

--- Step 1.3/vector erass.cpp
+++ Step 1.3/vector erass.cpp
@@ -3,12 +3,18 @@
 using namespace std;
 
 int main(){
-    vector<int> v = {1,2,3,4};
-    v.erase(v.begin());
-    cout<<endl;
+    vector<int> v = {0,1,2,3,4,5,6,7,8,9};
+    // v.erase(v.begin());
+    // cout<<endl;
     cout<<"using for each loop : ";
     for(auto it2 : v){
         cout<<it2<<" ";
+    }
+    cout<<endl;
+    v.erase(v.begin()+3,v.begin()+7);
+    cout<<endl;
+    for(auto it3 : v){
+        cout<<it3<<" ";
     }
     cout<<endl;
 }
