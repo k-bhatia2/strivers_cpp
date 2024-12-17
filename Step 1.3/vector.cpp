997cb0142e731d1e33759bf3c877d0ff57e587e0
k-bhatia2 <kshitijbhatia02@gmail.com>
2024-12-17 20:10:28 +0530

    iterations of vectors

--- Step 1.3/vector.cpp
+++ Step 1.3/vector.cpp
@@ -4,9 +4,15 @@ using namespace std;
 
 int main(){
     vector<int> v = {1,2,3,4};
-    cout<<"v[3] = "<<v[3]<<endl;
+    cout<<"v[2] = "<<v[2]<<endl;
     vector<int>::iterator it=v.begin();
     cout<<"iterator = "<<*it<<endl;
     it++;
     cout<<"iterator updated= "<<*it<<endl;
+    cout<<"v.back = "<<v.back()<<endl;
+    cout<<"vector = ";
+    for(vector<int>::iterator it1 = v.begin(); it1 !=v.end();it1++){
+        cout<<*it1<<",";
+    }
+    cout<<endl;
 }
